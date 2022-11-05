a92c7351120ced8be0a8500e9cb12b39caaf9af1
VMLSousa <61851284+VMLSousa@users.noreply.github.com>
2022-11-05 17:34:06 +0000

    Update User.cs

--- ProjetoNatal/Client/User.cs
+++ ProjetoNatal/Client/User.cs
@@ -2,6 +2,7 @@
 {
     public class User
     {
+        
         public int id { get; set; }
         public string nome { get; set; } = string.Empty;
         public string alvo { get; set; } = string.Empty;
